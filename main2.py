2041351a6d1bc0bdbf7fd3aa489d7e95aa51a96d
Rojas-Andres <andresfelipe200004@gmail.com>
2023-03-01 21:47:24 -0500

    implementacion

--- main2.py
+++ main2.py
@@ -2,9 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 from lxml import etree
 
-def get_full_data_down(soup, li_xpath):
+def get_full_data_down(soup, li_xpath, index = None):
     list_elements = []
-    elements = soup.find('li',attrs={"class":li_xpath}).find('ul').find_all('li')
+    if index is None:
+        elements = soup.find('li',attrs={"class":li_xpath}).find('ul').find_all('li')
+    else:
+        elements = soup.find_all('li',attrs={"class":li_xpath})[index].find('ul').find_all('li')
     for ele in elements:
         a = ele.find('a')
         data = {
@@ -13,12 +16,13 @@ def get_full_data_down(soup, li_xpath):
             "img": a.find('img')["src"]
         }
         list_elements.append(data)
-        
     return list_elements
 
 def get_data_down(soup):
     down = soup.find('ul',attrs={"class":"list-no-style row large-up-nowrap"})
     universities = get_full_data_down(down,"border-box width-100 large-up-width-3-7 padding-horz-small relative")
+    providers = get_full_data_down(down,"medium-down-hidden decor-grid-line border-box width-100 large-up-width-2-7 padding-horz-small large-up-padding-horz-large relative", 0)
+    institutions = get_full_data_down(down,"medium-down-hidden decor-grid-line border-box width-100 large-up-width-2-7 padding-horz-small large-up-padding-horz-large relative", 1)
     pass
 
 def get_data():
